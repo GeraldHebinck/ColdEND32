98276f3ebeb2614b725b622c676a9325dc34d03d
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2022-05-22 01:36:04 +0200

    simplified adc conversion

--- include/vars.h
+++ include/vars.h
@@ -20,11 +20,12 @@ unsigned long   prev_pot_read = 0;                      // Previous pot read loo
 unsigned long   curr_pot_read = 0;                      // Current pot read loop
 float           max_spit = MAX_SPIT_TIME;               // Convert MAX_SPIT_TIME to float
 #ifdef COLDEND32
-float           exp_scale = log(MAX_RPM/MIN_RPM)/4095;  // Exponential flow scaling to match a 12bit analogue value
+unsigned int    adc_resolution = 4095;
 #endif
 #ifdef MKS_GENL
-float           exp_scale = log(MAX_RPM/MIN_RPM)/1023;  // Exponential flow scaling to match a 10bit analogue value
+unsigned int    adc_resolution = 1023;
 #endif
+float           exp_scale = log(MAX_RPM/MIN_RPM)/adc_resolution;  // Exponential flow scaling to match a 12bit analogue value
 
 
 // Relais states
